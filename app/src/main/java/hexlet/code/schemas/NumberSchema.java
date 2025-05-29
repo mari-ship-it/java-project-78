29101374dbea58a49129aebd137254b7e5de033e
Marina Sirenko <msirenko96@gmail.com>
2025-05-30 00:22:35 +0300

    add NumberSchema.java

---
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -0,0 +1,21 @@
+package hexlet.code.schemas;
+
+import java.util.Objects;
+
+public class NumberSchema extends BaseSchema<Integer> {
+
+    public NumberSchema required() {
+        addValidation("required", Objects::nonNull);
+        return this;
+    }
+
+    public NumberSchema positive() {
+        addValidation("positive", obj -> obj > 0);
+        return this;
+    }
+
+    public NumberSchema renge(int min, int max) {
+        addValidation("renge", obj -> obj >= min && obj <= max);
+        return this;
+    }
+}
