c07d2ac503b83f7d25beb17836db493ee8b9870e
Marina Sirenko <msirenko96@gmail.com>
2025-05-30 00:08:32 +0300

    add StringSchemaTest.java

---
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -0,0 +1,69 @@
+package hexlet.code;
+
+import hexlet.code.schemas.StringSchema;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringSchemaTest {
+
+    StringSchema schema;
+
+    @BeforeEach
+    public void beforeEach() {
+        Validator validator = new Validator();
+        schema = validator.string();
+    }
+
+    @Test
+    public void nullAndIsEmptyTest() {
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(""));
+    }
+
+    @Test
+    public void requiredTest() {
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(""));
+        assertTrue(schema.isValid("what does the fox say"));
+        assertTrue(schema.isValid("hexlet"));
+    }
+
+    @Test
+    public void minLengthTest() {
+
+        schema.minLength(10).minLength(4);
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid("hexlet"));
+    }
+
+    @Test
+    public void containsTest() {
+
+        schema.contains("wh");
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid("what does the fox say"));
+
+        schema.contains("whatthe");
+        assertFalse(schema.isValid("what does the fox say"));
+    }
+
+    @Test
+    public void setOfChecksTest() {
+
+        schema.required().minLength(5).contains("hex");
+
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(""));
+        assertTrue(schema.isValid("hexlet"));
+    }
+}
